b535f2cf7f5f1fc17a85599ff37e6d8123b18c2a
Ulysse Mizrahi <odusseys@gmail.com>
2019-03-01 10:16:48 +0100

    adds parsing of config from environment

--- api/api/config.py
+++ api/api/config.py
@@ -1,14 +1,31 @@
 import os
 import re
 
+CONFIG_KEYS = [
+    "USER_AUTH_SECRET",
+    "ADMIN_AUTH_SECRET",
+    "SQLALCHEMY_DATABASE_URI",
+    "AWS_DEFAULT_REGION",
+    "AWS_ACCESS_KEY_ID",
+    "AWS_SECRET_ACCESS_KEY"
+]
+
 config = {}
 
+parsed_config = {}
 try:
     folder = os.path.dirname(__file__)
     with open(os.path.join(folder, ".env")) as env_file:
         for line in env_file.readlines():
             match = re.search("^([^=]+)=(.+)$", line)
-            config[match.group(1)] = match.group(2)
-        print(config)
+            parsed_config[match.group(1)] = match.group(2)
 except:
-    raise ValueError("No .env file found in api folder")
+    pass
+for key in CONFIG_KEYS:
+    if key in os.environ:
+        config[key] = os.environ[key]
+    elif key in parsed_config:
+        config[key] = parsed_config[key]
+    else:
+        raise ValueError(f"Missing key in environment: {key}")
+print("Parsed config", config)
